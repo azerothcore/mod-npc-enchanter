394a239b891ccb64b558875b64239e64c5e8f041
Vitaliy <mitradis@gmail.com>
2021-03-02 14:11:50 +0300

    Update npc_enchanter.cpp

--- src/npc_enchanter.cpp
+++ src/npc_enchanter.cpp
@@ -207,16 +207,6 @@ public:
     void OnBeforeConfigLoad(bool reload) override
     {
         if (!reload) {
-            std::string conf_path = _CONF_DIR;
-            std::string cfg_file = conf_path + "/npc_enchanter.conf";
-
-#ifdef WIN32
-            cfg_file = "npc_enchanter.conf";
-#endif
-
-            std::string cfg_def_file = cfg_file + ".dist";
-            sConfigMgr->LoadMore(cfg_def_file.c_str());
-            sConfigMgr->LoadMore(cfg_file.c_str());
             EnchanterAnnounceModule = sConfigMgr->GetBoolDefault("Enchanter.Announce", 1);
             EnchanterNumPhrases = sConfigMgr->GetIntDefault("Enchanter.NumPhrases", 3);
             EnchanterMessageTimer = sConfigMgr->GetIntDefault("Enchanter.MessageTimer", 60000);
